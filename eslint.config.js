e12d492986b2344e86b629a183e7245a354567fa
carrot <viktoriya.korekina@simpleone.ru>
2026-05-02 18:29:23 +0300

    feat: additional setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reactPlugin from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
@@ -16,6 +18,9 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat["jsx-runtime"],
+      eslintConfigPrettier,
     ],
     languageOptions: {
       globals: globals.browser,
