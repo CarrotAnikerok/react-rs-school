f6bed97e0ca27ced874e1cd3663140ebbda4213c
carrot <viktoriya.korekina@simpleone.ru>
2026-05-10 21:38:01 +0300

    init: set configuration

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(viteConfig, defineConfig({
+    test: {
+        exclude: ['node_modules', 'packages/template/*'],
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './tests/test-utils/setup.ts',
+        coverage: {
+            thresholds: {
+                global: {
+                    statements: 80,
+                    branches: 50,
+                    functions: 50,
+                    lines: 50,
+                },
+            },
+        }
+    }
+}))
