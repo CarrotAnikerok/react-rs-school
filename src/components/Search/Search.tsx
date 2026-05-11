661443a635c0fac76200e2d747fa59a5361677c6
carrot <viktoriya.korekina@simpleone.ru>
2026-05-11 19:05:33 +0300

    fix: format:fix

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,54 +1,63 @@
-import { Component, type ReactNode, type SubmitEvent } from "react";
-import "./Search.css"
+import { Component, type ReactNode, type SubmitEvent } from 'react';
+import './Search.css';
 
 type SearchProps = {
-    children?: ReactNode
-    onSearch: (query?:string) => void;
-}
+  children?: ReactNode;
+  onSearch: (query?: string) => void;
+};
 
 type SearchState = {
-    value: string
-}
+  value: string;
+};
 
 export class Search extends Component<SearchProps, SearchState> {
-    constructor(props: SearchProps) {
-        super(props);
-        this.state = {
-            value: localStorage.getItem('searchValue') || '',
-        }
-    }
+  constructor(props: SearchProps) {
+    super(props);
+    this.state = {
+      value: localStorage.getItem('searchValue') || '',
+    };
+  }
 
-    handleSubmit = (event: SubmitEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const value = formData.get('query')?.toString() || '';
-        this.setState({ value: value.trim()});
-    }
+  handleSubmit = (event: SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const value = formData.get('query')?.toString() || '';
+    this.setState({ value: value.trim() });
+  };
 
-    componentDidMount(): void {
-        if (this.state.value) {
-            this.props.onSearch(this.state.value.replaceAll(' ', '_'));
-        } else {
-            this.props.onSearch();
-        }
+  componentDidMount(): void {
+    if (this.state.value) {
+      this.props.onSearch(this.state.value.replaceAll(' ', '_'));
+    } else {
+      this.props.onSearch();
     }
+  }
 
-    componentDidUpdate(_prevProps: Readonly<SearchProps>, prevState: Readonly<SearchState>): void {
-        if (this.state.value !== prevState.value) {
-            this.props.onSearch(this.state.value.replaceAll(' ', '_') || 'all');
-            localStorage.setItem('searchValue', this.state.value);
-        }
+  componentDidUpdate(
+    _prevProps: Readonly<SearchProps>,
+    prevState: Readonly<SearchState>
+  ): void {
+    if (this.state.value !== prevState.value) {
+      this.props.onSearch(this.state.value.replaceAll(' ', '_') || 'all');
+      localStorage.setItem('searchValue', this.state.value);
     }
+  }
 
-    render() {
-        return <form onSubmit={this.handleSubmit}>
-            <label htmlFor="search">Search the pony!</label>
-            <div className="input-group">
-                <input id="search" name="query" type="search" placeholder="rarity" defaultValue={this.state.value}></input>
-                <button type="submit">Search</button>
-            </div>
-        </form>
-    }
-
-    
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="search">Search the pony!</label>
+        <div className="input-group">
+          <input
+            id="search"
+            name="query"
+            type="search"
+            placeholder="rarity"
+            defaultValue={this.state.value}
+          ></input>
+          <button type="submit">Search</button>
+        </div>
+      </form>
+    );
+  }
 }
