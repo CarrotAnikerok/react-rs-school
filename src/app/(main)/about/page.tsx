7c68eb499fbce718e46b5f66a69a1128217e688a
carrot <viktoriya.korekina@simpleone.ru>
2026-06-20 17:20:28 +0300

    fix: change about/page

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -1,13 +1,14 @@
+import Link from 'next/link';
 import './About.css';
 
 export default function About() {
   return (
     <div className="about">
-      <p>Hello! Im Carrot and i&apos;m author of this website!</p>
+      <p>Hello! I&apos;m Carrot and  author of this website!</p>
       <p>My github: CarrotAnikerok</p>
       <p>My discord: carrotanikerok</p>
       <p>
-        from <a href="https://rs.school/courses/reactjs">react school course</a>
+        from <Link href="https://rs.school/courses/reactjs">react school course</Link>
         !
       </p>
     </div>
